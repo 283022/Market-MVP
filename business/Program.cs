8c0653e0657c698b05df3539faa36e33b21d8afd
Artem <bafara032@gmail.com>
2026-08-27 11:33:59 +0300

    few: write business service

---
+++ business/Program.cs
@@ -0,0 +1,46 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenApi();
+var secret = builder.Configuration.GetSection("AppSettings:Token").Value;
+var issuer = builder.Configuration["JwtIssuer"];
+var audience = builder.Configuration["JwtAudience"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+            ClockSkew = TimeSpan.FromSeconds(5) // Допуск на рассинхрон часов
+        };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/info", () => "hi").RequireAuthorization();
+app.MapGet("/business",
+    (HttpContext context) =>
+    {
+        var claim = context.User.Claims;
+        return Results.Ok(claim);
+    }).RequireAuthorization();
