9dd5248c51dfc6b51d68f0d49a2404b2d213514d
Artem <bafara032@gmail.com>
2026-09-03 20:28:34 +0300

    few: add order service

---
+++ OrderServices/Endpoints.cs
@@ -0,0 +1,38 @@
+﻿namespace OrderServices;
+
+public static class Endpoints
+{
+    public static WebApplication BuildWebApplication(this WebApplication app)
+    {
+        var group = app.MapGroup("/api/orders");
+        group.MapGet("/my", () =>
+        {
+
+        });
+        group.MapPost("/create", () =>
+        {
+
+        });
+        group.MapGet("/{id}", () =>
+        {
+
+        });
+
+        group.MapPatch("/{id}/cancel", () =>
+        {
+
+        });
+
+        group.MapPost("/{id}/payment-confirm", () =>
+        {
+
+        });
+
+        group.MapGet("/{id}/status", () =>
+        {
+
+        });
+        
+        return app;
+    }
+}
